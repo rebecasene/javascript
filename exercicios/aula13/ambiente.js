e9704d613c54102767383a3c13a7cc52ca2ffb5e
Rebeca Sene <rebecabsene@gmail.com>
2026-04-30 17:42:52 -0300

    atualizações

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,11 @@
+let pizza = 8 
+let qp = 0
+
+    do {
+        pizza--
+        qp ++
+        console.log(`comi o meu ${qp}° pedaço`)
+        } while (pizza > 0)
+
+
+
---
+++ exercicios/aula14/amientet.js
@@ -0,0 +1,3 @@
+for (let pizza = 8; pizza > 0; pizza--) {
+    console.log('comi')
+}
